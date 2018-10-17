cfd22fdf7b1d77646a6322567945a0c637b39490
cwijayasundara <cwijayasundara@gmail.com>
2018-10-17 10:01:51 +0100

    Fixed the issue

--- src/main/java/com/cham/springintegrationjms/SpringIntegrationJmsApplication.java
+++ src/main/java/com/cham/springintegrationjms/SpringIntegrationJmsApplication.java
@@ -1,22 +1,20 @@
 package com.cham.springintegrationjms;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.messaging.MessageChannel;
 
 @SpringBootApplication
-/*@ImportResource({"common.xml","inboundGateway.xml","outboundGateway.xml", "inboundChannelAdapter.xml",
-		"outboundChannelAdapter.xml", "aggregation.xml"})*/
+@ImportResource({"common.xml", "aggregation.xml"})
 public class SpringIntegrationJmsApplication  implements CommandLineRunner {
 
-	private final static String[] configFilesAggregationDemo = {
-			"common.xml",
-			"aggregation.xml"
-
-	};
+	@Autowired
+	private ApplicationContext appContext;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIntegrationJmsApplication.class, args);
@@ -24,10 +22,8 @@ public class SpringIntegrationJmsApplication  implements CommandLineRunner {
 
 	@Override
 	public void run(String... strings) throws Exception {
-		System.out.println("    Loading Aggregation Demo...");
-		final ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(configFilesAggregationDemo, SpringIntegrationJmsApplication.class);
-		final MessageChannel stdinToJmsOutChannel = applicationContext.getBean("stdinToJmsOutChannel", MessageChannel.class);
-		stdinToJmsOutChannel.send(MessageBuilder.withPayload("JMS test").build());
-		stdinToJmsOutChannel.send(MessageBuilder.withPayload("kafka test").build());
+		final MessageChannel stdinToJmsOutChannel = appContext.getBean("stdinToJmsOutChannel", MessageChannel.class);
+		stdinToJmsOutChannel.send(MessageBuilder.withPayload("JMS message").build());
+		stdinToJmsOutChannel.send(MessageBuilder.withPayload("kafka message").build());
 	}
 }
